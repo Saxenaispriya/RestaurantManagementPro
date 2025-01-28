7eeb698b41b2ae7489efc690fb305e0294173503
Priyanka Saxena <saxena23priya@gmail.com>
2025-01-29 00:33:18 +0530

    add new coomit for dictionary use

--- Restaurant.cs
+++ Restaurant.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,111 +7,111 @@ namespace RestaurantManagementPro
 {
     internal class Restaurant
     {
-       public List<fooditem> fooditemlst = new List<fooditem>();
-       public List<table> tablelist = new List<table>();
+        Dictionary<int, fooditem> mydictfooditem = new Dictionary<int, fooditem>();
+        Dictionary<int, table> mydicttable = new Dictionary<int, table>();
 
 
         public void initMenu()
         {
-           var itemList = new List<string>(){
+            var itemList = new List<string>(){
                 "No food", "Dal Fry", "Rice", "Kadhai Panner"
             };
 
-           var foodPriceList = new List<int>(){
+            var foodPriceList = new List<int>(){
              0,100, 200, 400
             };
 
-            for (int i =0; i< itemList.Count; i++){
-                fooditemlst.Add(new fooditem() { foodItemId = i, foodItemName = itemList[i], foodPrice = foodPriceList[i] });
-
+            for (int i = 0; i < itemList.Count; i++)
+            {
+                fooditem foodItem = new fooditem();
+                foodItem.foodItemId = i;
+                foodItem.foodItemName = itemList[i];
+                foodItem.foodPrice = foodPriceList[i];
+                mydictfooditem.Add(i, foodItem);
             }
         }
 
         public void initTables()
         {
-            for (int i = 1; i<= 10; i++ ) {
-                tablelist.Add(new table() { tableNumber = i, isTableOccupied = false });
-            }
-        }
-
-        public void addTable() {
-            Console.WriteLine("Add table number in the List");
-            int _tableNumber=Convert.ToInt32(Console.ReadLine());
-            tablelist.Add(new table()
-            { tableNumber= _tableNumber, isTableOccupied = false});
-        }
-
-        public void removeTable() {
-            Console.WriteLine("which table number you want to delete");
-            int _tablenumber=Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            foreach (table t in tablelist.ToList())
+            for (int i = 1; i <= 10; i++)
             {
-                if(t.tableNumber.Equals(_tablenumber) && t.isTableOccupied==false)
-                {
-                    tablelist.RemoveAt(i); 
-                }
-                i++;
+                table t = new table();
+                t.tableNumber = i;
+                t.isTableOccupied = false;
+                mydicttable.Add(i, t);
             }
+        }
 
+        public void addTable()
+        {
+            Console.WriteLine("Add table number in the List");
+            int _tableNumber = Convert.ToInt32(Console.ReadLine());
+            table t = new table();
+            t.tableNumber = _tableNumber;
+            t.isTableOccupied = false;
+            mydicttable.Add(_tableNumber, t);
+        }
+
+        public void removeTable()
+        {
+            Console.WriteLine("which table number you want to delete");
+            int _tablenumber = Convert.ToInt32(Console.ReadLine());
+            if (mydicttable.ContainsKey(_tablenumber))
+            {
+                mydicttable.Remove(_tablenumber);
+            }
         }
 
         public void removeFooditem()
         {
             Console.WriteLine("which food item Id you want to delete");
-            int _fooditem=Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            foreach(fooditem f in fooditemlst.ToList())
+            int _fooditem = Convert.ToInt32(Console.ReadLine());
+            if (mydictfooditem.ContainsKey(_fooditem))
             {
-                if(f.foodItemId==_fooditem)
-                {
-                    fooditemlst.RemoveAt(i);
-                }
-                i++;
+                mydictfooditem.Remove(_fooditem);
             }
         }
 
         public void showAvailableTables()
         {
-            foreach (table t in tablelist)
+            foreach (KeyValuePair<int, table> t in mydicttable)
             {
-                if (!t.isTableOccupied)
+                if (!t.Value.isTableOccupied)
                 {
-                    Console.WriteLine(t.tableNumber + "\t");
+                    Console.WriteLine(t.Value.tableNumber + "\t");
                 }
             }
         }
 
         public void showMenuesItem()
         {
-            foreach (fooditem f in fooditemlst)
-            { 
-                Console.Write("food Id:" +  f.foodItemId +"\t");
-                Console.Write("," +"food name: " + f.foodItemName +"\t");
-                Console.WriteLine("," + "food Price: " + f.foodPrice);
+            foreach (KeyValuePair<int, fooditem> f in mydictfooditem)
+            {
+                Console.Write("food Id:" + f.Value.foodItemId + "\t");
+                Console.Write("," + "food name: " + f.Value.foodItemName + "\t");
+                Console.WriteLine("," + "food Price: " + f.Value.foodPrice);
             }
         }
 
         public void Do_Orders(int tableNumber)
         {
-            table t = new table();
             showMenuesItem();
-            bool orderComplete=true;
-            while (orderComplete) {
+            bool orderComplete = true;
+            while (orderComplete)
+            {
                 Console.WriteLine("enter the item number");
                 int itemNumber = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("enter the quntity");
-            int quantity=Convert.ToInt32(Console.ReadLine());
+                int quantity = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i<tablelist.Count; i++) {
-                if (tablelist[i].tableNumber == tableNumber) {
-                        tablelist[i].orderlist.Add(new order(itemNumber, quantity));
-                        tablelist[i].isTableOccupied = true;
+                if (mydicttable.ContainsKey(tableNumber))
+                {
+                    table t = mydicttable[tableNumber];
+                    t.orderlist.Add(new order(itemNumber, quantity));
                 }
-            }
-               Console.WriteLine("Do you want to add more items; just type Y or N");
-               string  inp = Console.ReadLine();
-                if(inp.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Do you want to add more items; just type Y or N");
+                string inp = Console.ReadLine();
+                if (inp.Equals("Y", StringComparison.OrdinalIgnoreCase))
                 {
                     orderComplete = true;
                 }
@@ -124,27 +125,28 @@ namespace RestaurantManagementPro
         }
 
         public void generateBill()
-        {   
+        {
             Console.WriteLine("Eneter the table number");
-            int tableNumber = Convert.ToInt32 (Console.ReadLine());
+            int tableNumber = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
-            foreach (table t in tablelist)
+            if(mydicttable.ContainsKey(tableNumber))
             {
-               if(t.tableNumber == tableNumber) {
-                    foreach (order or in t.orderlist)
+                table t=mydicttable[tableNumber];
+                foreach (order or in t.orderlist)
+                {
+                    if(mydictfooditem.ContainsKey(or._fooditemNumber))
                     {
-                        foreach (fooditem fd in fooditemlst)
-                        {
-                            if (fd.foodItemId == or._fooditemNumber) {
-                                sum += fd.foodPrice * or._qunatiy;
-                            }
-                        }
+                        fooditem fditm=mydictfooditem[or._fooditemNumber];
+                        sum += fditm.foodPrice * or._qunatiy;
                     }
                 }
+            } else
+            {
+                Console.WriteLine("table not found");
             }
+
             Console.WriteLine("you need to pay amount:" + sum);
         }
-
 
     }
 }
